ea67503287b93bdf37b71a78945b11f45ce2f341
Connor Francis <connorfrancis@live.com>
2025-12-13 19:31:43 -0700

    finished game

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
-let playerMove = prompt("Make a move: ")
+let playerMove = ''
+
+let gamesPlayed = 0
+
+let wonGames = 0
 
 
 function rpsGame(playerMove){
@@ -25,10 +29,14 @@ function rpsGame(playerMove){
     if(machineChoice === playerMove){
         alert('Verdict: Tie')
     }else if (playerMove === "Rock" && machineChoice==="Scissors"){
+        wonGames += 1
         alert('Verdict: You Win!')
     }else if(playerMove === "Paper" && machineChoice==="Rock"){
+        wonGames += 1
         alert('Verdict: You Win!')
+
     }else if (playerMove === "Scissors" && machineChoice==="Paper"){
+        wonGames += 1
         alert('Verdict: You Win!')
     }else{
         alert('Verdict: You lose :(')
@@ -37,12 +45,26 @@ function rpsGame(playerMove){
 }
      
 
-// calling the game function with player's input as argument
-rpsGame(playerMove)
+//for loop to play 5 games
+
+for(i=0;gamesPlayed<5;i++){
+
+    playerMove = prompt("Make a move: ")
+
+    // calling the game function with player's input as argument
+    rpsGame(playerMove)
+
+    gamesPlayed += 1
+
+    if(gamesPlayed === 5 && wonGames > 2){
+        alert("You are the champion!")
+    } else if(gamesPlayed === 5){
+        alert(`You suck at this game... Won games: ${wonGames}`)
+    }
+}
+
+
     
-
-
-
 
 
 
